815ccbf11ec3fc6e1f3bf06b1b9e858f99f9fa52
cd.hong <hcd0923@gmail.com>
2017-02-07 02:16:09 +0900

    ccpp 동작 명확치 않음 확인 필요

--- test/src/test/java/test/applicationcontext/annotation/TestAnnotationApplicationContext.java
+++ test/src/test/java/test/applicationcontext/annotation/TestAnnotationApplicationContext.java
@@ -1,28 +1,51 @@
 package test.applicationcontext.annotation;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.AnnotatedGenericBeanDefinition;
+import org.springframework.beans.factory.annotation.QualifierAnnotationAutowireCandidateResolver;
+import org.springframework.beans.factory.support.BeanDefinitionRegistry;
+import org.springframework.beans.factory.support.DefaultListableBeanFactory;
 import org.springframework.context.annotation.AnnotatedBeanDefinitionReader;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.AnnotationScopeMetadataResolver;
 import org.springframework.context.annotation.ClassPathBeanDefinitionScanner;
+import org.springframework.context.annotation.ConfigurationClassPostProcessor;
 import org.springframework.context.annotation.ScopeMetadata;
 import org.springframework.context.annotation.ScopeMetadataResolver;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.core.env.StandardEnvironment;
 
 import test.controller.TestController;
 import test.service.impl.TestServiceImpl;
 
 public class TestAnnotationApplicationContext {
 	Logger logger = LoggerFactory.getLogger(TestAnnotationApplicationContext.class);
+	private DefaultListableBeanFactory beanFactory;
+	
+	@Before
+	public void setUp() {
+		DefaultListableBeanFactory bf = new DefaultListableBeanFactory();
+		QualifierAnnotationAutowireCandidateResolver acr = new QualifierAnnotationAutowireCandidateResolver();
+		acr.setBeanFactory(bf);
+		bf.setAutowireCandidateResolver(acr);
+		this.beanFactory = bf;
+	}
 	
 	@Test
-	public void regsterAnnotatedBeanTest(){
+	public void diriectRegsterAnnotatedBeanTest(){
 		AnnotationConfigApplicationContext acac = new AnnotationConfigApplicationContext();
 		acac.register(TestController.class);
 		acac.register(TestServiceImpl.class);
+		acac.refresh();
+	}
+	
+	@Test
+	public void configrationRegsterAnnotatedBeanTest(){
+		AnnotationConfigApplicationContext acac = new AnnotationConfigApplicationContext();
+		acac.register(TestAnnotationConfigue.class);
 		acac.refresh();
 	}
 	
@@ -31,27 +54,6 @@ public class TestAnnotationApplicationContext {
 		AnnotationConfigApplicationContext acac = new AnnotationConfigApplicationContext();
 		acac.scan("test");
 		acac.refresh();
-	}
-	
-	
-//	ClassPathScanningCandidateComponentProvider.findCandidateComponents
-	@Test
-	public void xmlAnnotationTest(){
-		ClassPathXmlApplicationContext ac = new ClassPathXmlApplicationContext("spring/annotation/application-config.xml");
-		ac.refresh();
-	}
-	
-	
-	@Test
-	public void getAnnotatedBeanDefinitionReaderTest(){
-		AnnotatedBeanDefinitionReader reader = new AnnotatedBeanDefinitionReader(new AnnotationConfigApplicationContext());
-		reader.register(TestAnnotationConfigue.class);
-	}
-	
-	@Test
-	public void getClassPathBeanDefinitionScannerTest(){
-		ClassPathBeanDefinitionScanner scanner = new ClassPathBeanDefinitionScanner(new AnnotationConfigApplicationContext());
-		scanner.scan("test");
 	}
 	
 	@Test
@@ -64,6 +66,48 @@ public class TestAnnotationApplicationContext {
 	}
 	
 	
+//	ClassPathScanningCandidateComponentProvider.findCandidateComponents
+	@Test
+	public void xmlAnnotationTest(){
+		ClassPathXmlApplicationContext ac = new ClassPathXmlApplicationContext("spring/annotation/application-config.xml");
+		ac.refresh();
+	}
+	
+	
+//	AbstractApplicationContext.invokeBeanFactoryPostProcessors
+	@Test
+	public void getAnnotatedBeanDefinitionReaderTest(){
+		AnnotatedBeanDefinitionReader reader = new AnnotatedBeanDefinitionReader(new AnnotationConfigApplicationContext());
+		reader.register(TestAnnotationConfigue.class);
+		BeanDefinitionRegistry br = reader.getRegistry();
+		for (String beanDefinitionName : br.getBeanDefinitionNames()) {
+			logger.debug(beanDefinitionName);
+		}
+		
+		logger.debug(String.valueOf(br.getBeanDefinitionCount()));
+		
+		ConfigurationClassPostProcessor ccpp = new ConfigurationClassPostProcessor();
+		ccpp.setEnvironment(new StandardEnvironment());
+		ccpp.postProcessBeanFactory(beanFactory);
+		ccpp.postProcessBeanDefinitionRegistry(br);
+		
+		//TODO : more study
+		beanFactory.preInstantiateSingletons();
+//		beanFactory.getBean(TestController.class);
+		
+		
+	}
+	
+	@Test
+	public void getClassPathBeanDefinitionScannerTest(){
+		ClassPathBeanDefinitionScanner scanner = new ClassPathBeanDefinitionScanner(new AnnotationConfigApplicationContext());
+		scanner.scan("test");
+	}
+	
+
+	
+	
 	
 	
 }
+
